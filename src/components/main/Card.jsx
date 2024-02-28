d1229130813afbff14df38e2c430cf0bae4be51c
Nicolastolinii <nicolas_tolini@hotmail.com>
2024-02-27 22:09:18 -0300

    deploy

--- src/components/main/Card.jsx
+++ src/components/main/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({ blog, showContent = true, w, flex, mb, imgh, imgmargin, padd, ce
     <div className={`w-full ${w} ${mb} ${flex} bg-white rounded-xl shadow-custom p-4 transition transform hover:-translate-y-2 hover:-translate-x-2 hover:scale-105`}>
       
       <Link to={href}>
-        <img loading="lazy" className={`bg-cover object-cover w-full rounded-lg ${imgh}  ${imgmargin}`} src={`data:image;base64,${blog.imagen}`} alt={blog.title} />
+        <img loading="lazy" alt="imagenes de portada de las tarjetas que representan cada post del blog" className={`bg-cover object-cover w-full rounded-lg ${imgh}  ${imgmargin}`} src={`data:image;base64,${blog.imagen}`}  />
       </Link>
       <div className={`font-openSans ${padd} ${centerText}  min-w-[280px] `}>
         <div>

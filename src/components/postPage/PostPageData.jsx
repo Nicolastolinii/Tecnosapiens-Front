2e66ddd6ccddabf75a7c6de352cd2020b8938ee8
Nicolastolinii <nicolas_tolini@hotmail.com>
2024-03-08 19:11:42 -0300

    deploy

--- src/components/postPage/PostPageData.jsx
+++ src/components/postPage/PostPageData.jsx
@@ -23,7 +23,7 @@ const PostPageData = () => {
                 <span>{formattedDate}</span>
                 <div>
                     <span>Categoria:</span>
-                    <span className="pl-1">IT</span>
+                    <span className="pl-1">{data?.categoria}</span>
                 </div>
             </div>
             
